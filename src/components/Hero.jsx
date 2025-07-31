c485fdec11e4c61f6be4738de78fb0df5bda7c7c
ryooz16 <riomardianto119@gmail.com>
2025-08-01 01:43:52 +0700

    membuat tampilan mobile untuk section hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,15 +11,16 @@ export default function Hero() {
     return (
         <section id="hero">
             <div className="relative flex flex-col justify-center items-center w-full h-screen font-montserrat">
-                <div className="w-3/6 flex flex-col gap-y-10">
-                    <div className="flex flex-col">
+                <div className="w-5/6 md:w-3/6 flex flex-col gap-y-10">
+                    {/* <img src="/assets/images/profile-picture.svg" alt="image" className="block md:hidden w-[150px] aspect-square border-2 border-white shadow-md rounded-xl" /> */}
+                    <div className="flex flex-col text-5xl md:text-7xl font-cormorant">
                         <div className="flex items-center gap-x-3">
-                            <h1 className="text-7xl font-cormorant">Hi, I'm Rio </h1>
-                            <img src="/assets/images/profile-picture.svg" alt="image" className="w-[90px] aspect-square rotate-[.25rad] border-2 border-white shadow-xl rounded-xl" />
+                            <h1 className="">Hi, I'm Rio </h1>
+                            <img src="/assets/images/profile-picture.svg" alt="image" className="w-[60px] md:w-[90px] aspect-square rotate-[.25rad] border-2 border-white shadow-xl rounded-xl" />
                         </div>
-                        <h1 className="text-7xl font-cormorant"> Permana <span className="text-blue-800">a Web Developer</span></h1>
+                        <h1 className=""> Permana <span className="text-blue-800">a Tech Enthusiast</span></h1>
                     </div>
-                    <p className="text-gray-700 font-medium">A web developer with ten years of hands-on experience, consistently delivering innovative and user-centered solutions</p>
+                    <p className="text-gray-700 font-medium">A fifth-semester Informatics Engineering student focusing on modern web development. He has a keen interest in building functional, responsive, and user-friendly web-based applications.</p>
                     <div className="flex items-center gap-x-5 font-medium">
                         <a href="mailto:helorio@gmail.com" className="px-5 py-3 bg-blue-800 rounded-lg text-white shadow-xl hover:opacity-80 transition duration-150">Talk with me</a>
                         <a href="" className="px-5 py-3 border border-gray-700 rounded-lg hover:bg-gray-700 hover:text-white transition-all duration-150">Download my cv</a>
